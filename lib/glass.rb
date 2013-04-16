634f8d0b53b3e28c6a877b11c6f17021c5ef9538
TheDamFr <dam.cavailles@gmail.com>
2013-04-15 22:50:20 -0400

    Added Mirror Object for holding secrets

--- lib/glass.rb
+++ lib/glass.rb
@@ -1,5 +1,18 @@
 require "glass/version"
 
 module Glass
-  # Your code goes here...
+
+  class Mirror
+    attr_accessor :scopes, :client_id, :redirect_uri, :cient_secret
+    @@client_id
+    @@cient_secret
+    @@redirect_uri
+    @@scopes  = [
+        'https://www.googleapis.com/auth/drive.file',
+        'https://www.googleapis.com/auth/userinfo.profile',
+    # Add other requested scopes.
+    ]
+
+  end
+
 end
