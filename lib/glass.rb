c6bfc7aea3d669f2e5dd4edf16b95c90f4411361
TheDamFr <dam.cavailles@gmail.com>
2013-04-15 23:14:09 -0400

    added class accessors

--- lib/glass.rb
+++ lib/glass.rb
@@ -3,15 +3,21 @@ require "glass/version"
 module Glass
 
   class Mirror
-    attr_accessor :scopes, :client_id, :redirect_uri, :cient_secret
+    class << self
+      attr_accessor :scopes, :client_id, :redirect_uri, :cient_secret
+    end
     @@client_id
     @@cient_secret
     @@redirect_uri
-    @@scopes  = [
+    @@scopes = [
         'https://www.googleapis.com/auth/drive.file',
         'https://www.googleapis.com/auth/userinfo.profile',
     # Add other requested scopes.
     ]
+
+    def self.hello_world
+      "Hello World!"
+    end
 
   end
 
